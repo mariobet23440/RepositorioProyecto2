5e2c2a44618f8fd4fdd7d1c014102cf64b5051a9
mariobet23440 <bet23440@uvg.edu.gt>
2025-05-03 19:44:19 -0600

    RECEPTOR - Documentación interna y librerías

---
+++ Proyecto2_Receptor/Proyecto2_Receptor/main.c
@@ -0,0 +1,23 @@
+/*
+* PROYECTO 2 RECEPTOR - PROGRAMACIÓN DE MICROCONTROLADORES
+* FECHA DE CREACIÓN: 3/05/25 19:35:08
+* FECHA DE ENTREGA: 17/05/25 16:30:00
+* AUTOR: Mario Alejandro Betancourt Franco (Universidad del Valle de Guatemala, 23440)
+* MICROCONTROLADOR: ATMega328P (En un Arduino Nano)
+* DESCRIPCIÓN: Programa para controlar un carro detector de obstáculos. El programa ofrece soporte para el control de 
+* dos motorreductores en las ruedas y dos servomotores. Adicionalmente utiliza un sensor ultrasónico para detectar obstáculos,
+* alertando de la presencia de objetos a una distancia menor a los 30 cm en la línea de visión (LOS). 
+*/ 
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
